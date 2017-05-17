84025a56c41ec6ff66c1e5f9a370a628d74c057b
Loïc Pinsard <muream@gmail.com>
2017-05-18 00:03:36 +0200

    Create init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,19 @@
+;; package management
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-At-startup nil)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-theme)
+(require 'init-evil)
