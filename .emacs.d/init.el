0d1c1b7f784ed82e90a485299ad9cf4cd0641c25
Loïc Pinsard <muream@gmail.com>
2017-05-19 00:36:34 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,5 +15,23 @@
   (require 'use-package))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
 (require 'init-theme)
 (require 'init-evil)
+(require 'init-projectile)
+(require 'init-python)
+(require 'init-company)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-jedi company company-mode projectile evil-surround evil-leader evil-escape evil linum-relative atom-one-dark-theme auto-complete use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Monaco" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
