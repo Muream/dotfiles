419d394e592000b8c877c08bea7cdb4e1bbd2fcf
Loïc Pinsard <muream@gmail.com>
2017-05-19 09:20:56 +0200

    Update init-company.el

--- .emacs.d/lisp/init-company.el
+++ .emacs.d/lisp/init-company.el
@@ -4,13 +4,12 @@
   (use-package company-jedi
     :ensure t
     :config
-    (add-to-list 'company-backends 'company-jedi))
-  (global-company-mode)
-  (global-set-key (kbd "C-SPC") 'company-complete)
-  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-  ;; (define-key company-active-map (kbd "S-TAB") 'company-select-next)
-  
+    (add-to-list 'company-backends 'company-jedi)
+    (global-company-mode)
+    (global-set-key (kbd "C-SPC") 'company-complete)
+    (define-key company-active-map (kbd "TAB") 'company-select-next)
+    (define-key company-active-map [tab] 'company-select-next))
+    (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
 )
-
 
 (provide 'init-company)
