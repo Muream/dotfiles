006505a3d2f76b05c392dffdfa9d1cd8fbf98434
muream <muream@gmail.com>
2017-09-04 11:52:30 +0200

    properly redirect stderr in stdout

--- i3/reddit-wallpaper.sh
+++ i3/reddit-wallpaper.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
 while true
 do 
-    WALLPAPER=$(/home/muream/.virtualenvs/reddit-wallpaper/bin/python ~/github/reddit-wallpaper/reddit-wallpaper.py >&1)
-    echo $WALLPAPER
+    WALLPAPER=$(/home/muream/.virtualenvs/reddit-wallpaper/bin/python ~/github/reddit-wallpaper/reddit-wallpaper.py 2>&1)
     feh --bg-scale $WALLPAPER
-    sleep 10
+    sleep 60
 done
