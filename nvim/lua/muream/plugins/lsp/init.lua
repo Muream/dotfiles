de3d36a78ea094b85151063bd5ca52b682d1d3f9
Loïc Pinsard <muream@gmail.com>
2022-09-05 15:25:56 +0200

    add clangd lsp config

--- nvim/lua/muream/plugins/lsp/init.lua
+++ nvim/lua/muream/plugins/lsp/init.lua
@@ -31,6 +31,12 @@ require("lspconfig").pyright.setup {
     capabilities = capabilities,
 }
 
+
+require("lspconfig").clangd.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
+
 require("lspconfig").sumneko_lua.setup {
     settings = {
         Lua = {
