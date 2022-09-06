1c14d0937a3d2840865b3965f52f0a8afea61376
Loïc Pinsard <muream@gmail.com>
2022-09-06 16:55:00 +0200

    include Windows Terminal config

--- install.ps1
+++ install.ps1
@@ -1,4 +1,15 @@
-New-Item -Force -ItemType SymbolicLink -Path $PROFILE -Value (Get-Item ".\Powershell\Microsoft.PowerShell_profile.ps1").FullName
-New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Local\nvim -Value (Get-Item ".\nvim").FullName
+# Symlink all the different configurations
+
+## Windows Terminal
+New-Item -Force -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Value (Get-Item ".\WindowsTerminal\settings.json").FullName
+New-Item -Force -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Value (Get-Item ".\WindowsTerminal\settings.json").FullName
+
+## Starship
 New-Item -Force -ItemType SymbolicLink -Path $HOME\.config\starship.toml -Value (Get-Item ".\starship\starship.toml").FullName
+
+## Powershell profile
+New-Item -Force -ItemType SymbolicLink -Path $PROFILE -Value (Get-Item ".\Powershell\Microsoft.PowerShell_profile.ps1").FullName
+
+## Neovim
+New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Local\nvim -Value (Get-Item ".\nvim").FullName
 
