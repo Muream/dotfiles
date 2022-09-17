f0790bd9223fc6964d6518271f4f6424bb1e8986
Loïc Pinsard <muream@gmail.com>
2022-09-17 16:24:54 +0200

    add poetry config

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
+ln -sf ~/projects/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
 ln -sf ~/projects/dotfiles/nvim ~/.config/nvim
+ln -sf ~/projects/dotfiles/pypoetry ~/.config/pypoetry
 ln -sf ~/projects/dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -sf ~/projects/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/projects/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
