292ffbf56c53a57f5f2498ce6ca6cdb380080668
Loïc Pinsard <muream@gmail.com>
2023-07-24 19:30:54 +0100

    update install.sh script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-ln -sf ~/projects/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
+ln -sf ~/projects/dotfiles/lazygit ~/.config/lazygit
 ln -sf ~/projects/dotfiles/nvim ~/.config/nvim
 ln -sf ~/projects/dotfiles/pypoetry ~/.config/pypoetry
 ln -sf ~/projects/dotfiles/starship/starship.toml ~/.config/starship.toml
