d4915786c6064d9d1573c990b9dee213a4375b63
Loïc Pinsard <muream@gmail.com>
2023-12-23 10:48:49 +0000

    stop using symbolic links to install dotfiles on windows

--- install.ps1
+++ install.ps1
@@ -1,18 +1,17 @@
 # Symlink all the different configurations
 
 ## Windows Terminal
-New-Item -Force -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Value (Get-Item ".\WindowsTerminal\settings.json").FullName
-New-Item -Force -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Value (Get-Item ".\WindowsTerminal\settings.json").FullName
+New-Item -Force -ItemType Junction -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -Value (Get-Item ".\WindowsTerminal").FullName
 
 ## Starship
-New-Item -Force -ItemType SymbolicLink -Path $HOME\.config\starship.toml -Value (Get-Item ".\starship\starship.toml").FullName
-
+New-Item -Force -ItemType HardLink -Path $HOME\.config\starship.toml -Value (Get-Item ".\starship\starship.toml").FullName
+ 
 ## Powershell profile
-New-Item -Force -ItemType SymbolicLink -Path $PROFILE -Value (Get-Item ".\Powershell\Microsoft.PowerShell_profile.ps1").FullName
+New-Item -Force -ItemType HardLink -Path $PROFILE -Value (Get-Item ".\Powershell\Microsoft.PowerShell_profile.ps1").FullName
 
 ## Neovim
-New-Item -Force -ItemType SymbolicLink -Path $HOME\AppData\Local\nvim -Value (Get-Item ".\nvim").FullName
+New-Item -Force -ItemType Junction -Path $HOME\AppData\Local\nvim -Value (Get-Item ".\nvim").FullName
 
 ## LazyGit
-New-Item -Force -ItemType SymbolicLink -Path "$env:APPDATA\lazygit\config.yml"  -Value (Get-Item ".\lazygit\config.yml").FullName
+New-Item -Force -ItemType HardLink -Path "$env:APPDATA\lazygit\config.yml"  -Value (Get-Item ".\lazygit\config.yml").FullName
 
