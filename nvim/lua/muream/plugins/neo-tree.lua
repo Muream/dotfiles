7b058a491920329b6cecbc9379ce4ad105e80e4b
Loïc Pinsard <muream@gmail.com>
2024-01-28 11:45:30 +0000

    fix missing icons in neo-tree

--- nvim/lua/muream/plugins/neo-tree.lua
+++ nvim/lua/muream/plugins/neo-tree.lua
@@ -23,5 +23,35 @@ return {
         window = {
             width = 35,
         },
+
+        default_component_configs = {
+            icon = {
+                folder_closed = "",
+                folder_open = "",
+                folder_empty = "",
+                folder_empty_open = "",
+                -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+                -- then these will never be used.
+                default = "*",
+                highlight = "NeoTreeFileIcon",
+            },
+
+            git_status = {
+                symbols = {
+                    -- Change type
+                    added = "✚", -- NOTE: you can set any of these to an empty string to not show them
+                    deleted = "✖",
+                    modified = "",
+                    renamed = "󰁕",
+                    -- Status type
+                    untracked = "",
+                    ignored = "",
+                    unstaged = "󰄱",
+                    staged = "",
+                    conflict = "",
+                },
+                align = "right",
+            },
+        },
     },
 }
