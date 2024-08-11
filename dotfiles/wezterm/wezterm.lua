b25974894ea5b8762a5639109c1fd286fec7b145
Loïc Pinsard <muream@gmail.com>
2024-08-11 19:57:27 +0100

    wezterm: add keybinds to run task commands

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -1,6 +1,33 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local function toggle_term(pane)
+    local tab = pane:tab()
+    local panes = tab:panes_with_info()
+    if #panes == 1 then
+        pane:split({
+            direction = "Bottom",
+            size = 0.3,
+        })
+    elseif panes[1].is_zoomed or panes[1].is_active then
+        tab:set_zoomed(false)
+        panes[2].pane:activate()
+    else
+        panes[1].pane:activate()
+        tab:set_zoomed(true)
+    end
+end
+
+local function run_command(pane, command)
+    toggle_term(pane)
+
+    local tab = pane:tab()
+    local panes = tab:panes_with_info()
+    tab:set_zoomed(false)
+    panes[2].pane:send_text(command .. "\r\n")
+end
+
+
 local config = {}
 config = wezterm.config_builder()
 
@@ -28,6 +55,7 @@ local function scheme_from_appearance(appearance)
 end
 
 config.automatically_reload_config = true
+config.window_close_confirmation = "NeverPrompt"
 
 -- Set the theme based on the system theme
 config.color_scheme = scheme_from_appearance(get_appearance())
@@ -53,7 +81,7 @@ config.keys = {
     { key = "s", mods = "LEADER",       action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }), },
     { key = "v", mods = "LEADER",       action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }), },
     { key = "z", mods = "LEADER",       action = "TogglePaneZoomState" },
-    { key = "x", mods = "LEADER",       action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+    { key = "x", mods = "LEADER",       action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
 
     -- Navigation
     { key = "h", mods = "CTRL",         action = act.EmitEvent("ActivatePaneDirection-left") },
@@ -83,7 +111,7 @@ config.keys = {
     { key = '}', mods = 'SHIFT|ALT',    action = act.MoveTabRelative(1) },
 
     { key = "c", mods = "LEADER",       action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-    { key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+    { key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
     {
         key = "f",
         mods = "LEADER",
@@ -98,29 +126,38 @@ config.keys = {
         key = "`",
         mods = "CTRL",
         action = wezterm.action_callback(function(_, pane)
-            local tab = pane:tab()
-            local panes = tab:panes_with_info()
-            if #panes == 1 then
-                pane:split({
-                    direction = "Bottom",
-                    size = 0.3,
-                })
-            elseif panes[1].is_zoomed then
-                tab:set_zoomed(false)
-                panes[2].pane:activate()
-            else
-                panes[1].pane:activate()
-                tab:set_zoomed(true)
-            end
+            toggle_term(pane)
         end),
     },
+    {
+        key = "b",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(_, pane)
+            run_command(pane, "task build")
+        end),
+    },
+
+    {
+        key = "r",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(_, pane)
+            run_command(pane, "task run")
+        end),
+    },
+    {
+        key = "t",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(_, pane)
+            run_command(pane, "task test")
+        end),
+    }
 }
 
 local function is_nvim(pane)
     return pane:get_user_vars().IS_NVIM == "true" or pane:get_foreground_process_name():find("n?vim")
 end
 
-local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+local function conditional_activate_pane(window, pane, pane_direction, vim_direction)
     if is_nvim(pane) then
         window:perform_action(
         -- This should match the keybinds you set in Neovim.
@@ -132,17 +169,23 @@ local function conditionalActivatePane(window, pane, pane_direction, vim_directi
     end
 end
 
-wezterm.on("ActivatePaneDirection-right", function(window, pane)
-    conditionalActivatePane(window, pane, "Right", "l")
-end)
-wezterm.on("ActivatePaneDirection-left", function(window, pane)
-    conditionalActivatePane(window, pane, "Left", "h")
-end)
-wezterm.on("ActivatePaneDirection-up", function(window, pane)
-    conditionalActivatePane(window, pane, "Up", "k")
-end)
-wezterm.on("ActivatePaneDirection-down", function(window, pane)
-    conditionalActivatePane(window, pane, "Down", "j")
-end)
+
+
+wezterm.on(
+    "ActivatePaneDirection-left",
+    function(window, pane) conditional_activate_pane(window, pane, "Left", "h") end
+)
+wezterm.on(
+    "ActivatePaneDirection-down",
+    function(window, pane) conditional_activate_pane(window, pane, "Down", "j") end
+)
+wezterm.on(
+    "ActivatePaneDirection-up",
+    function(window, pane) conditional_activate_pane(window, pane, "Up", "k") end
+)
+wezterm.on(
+    "ActivatePaneDirection-right",
+    function(window, pane) conditional_activate_pane(window, pane, "Right", "l") end
+)
 
 return config
