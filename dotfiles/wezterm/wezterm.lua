72707ebb5f150f1ac8d22c19b8896475af3f55ac
Loïc Pinsard <muream@gmail.com>
2025-09-03 21:36:08 +0100

    update font in wezterm

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -38,7 +38,7 @@ config.color_scheme = scheme_from_appearance(get_appearance())
 -- config.front_end = "WebGpu"
 config.prefer_egl = true
 config.max_fps = 60
--- config.font = wezterm.font("CaskaydiaCove Nerd Font")
+config.font = wezterm.font("Cascadia Code")
 config.font_size = 14.0
 
 -- General Options
