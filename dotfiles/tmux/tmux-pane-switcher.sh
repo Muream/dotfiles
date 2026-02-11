24a3a0382246b02b9ebd821588c5e9d45f60e8a2
Muream <muream@gmail.com>
2026-02-11 15:58:18 +0000

    use pane variables instead of changing the pane title when splitting/switching panes

--- dotfiles/tmux/tmux-pane-switcher.sh
+++ dotfiles/tmux/tmux-pane-switcher.sh
@@ -1,11 +1,11 @@
-PANE_TITLE="secondary_pane"
+PANE_ROLE="secondary_pane"
 
 # Try to find an existing pane with that title
-pane_id=$(tmux list-panes -F '#{pane_id} #{pane_title}' | awk -v t="$PANE_TITLE" '$2==t {print $1}')
+pane_id=$(tmux list-panes -F '#{pane_id} #{@role}' | awk -v t="$PANE_ROLE" '$2==t {print $1}')
 
 if [ -n "$pane_id" ]; then
   tmux select-pane -t "$pane_id"
 else
   tmux split-window
-  tmux select-pane -T "$PANE_TITLE"
+  tmux set-option -p @role "$PANE_ROLE"
 fi
